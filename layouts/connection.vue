a2a22951cc0868b61f0b62f8781c4aba82823adf
Clément <duvivier.clem@gmail.com>
2022-03-17 12:20:03 +0100

    :sparkles: layout for home

---
+++ layouts/connection.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <HeaderHome />
+        <main>
+            <Nuxt/>
+        </main>
+        <FooterHome />
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+    #app {
+        height: 100vh;
+        @include column;
+    }
+    main {
+        flex : 1;
+    }
+</style>
