2a5f2c0988623e81fc335ff21a6662c99f2b128f
Alexis Bally <alexisbally02@gmail.com>
2022-03-24 12:54:07 +0100

    Add one header component

---
+++ components/HeaderPrice.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="header-container">
+        <svg class="arrow-previous" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 242.133 242.133" style="enable-background:new 0 0 242.133 242.133;" xml:space="preserve"><path id="XMLID_24_" d="M190.919,212.133h-69.853c-8.284,0-15,6.716-15,15s6.716,15,15,15h106.065c8.284,0,15-6.716,15-15V121.066 c0-8.284-6.716-15-15-15s-15,6.716-15,15v69.854L25.607,4.394c-5.858-5.858-15.356-5.858-21.213,0 c-5.858,5.858-5.858,15.356,0,21.213L190.919,212.133z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+        <div class="header-text">
+            <p class="table">Table 23</p>
+            <h3 class="price">134,50€</h3>
+        </div>
+        <div class="header-links">
+            <nuxt-link :to="`/`" tag="button" class="btn btn-regular price-link">Régler ma part</nuxt-link>
+            <nuxt-link :to="`/`" tag="button" class="btn disabled" disabled>Partager l'addition</nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderPrice",
+    // add props
+    props : {
+        active: Boolean
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .header-container {
+        height: 18vh;
+        @include center-align;
+        justify-content: end;
+        flex-direction: column;
+        background-color: $c--secondary;
+        fill: white;
+
+        .arrow-previous {
+            width: 25px;
+            height: auto;
+            transform: rotate(135deg);
+            color: white;
+            position: absolute;
+            top: 30px;
+            left: 20px;
+        }
+
+        .header-text {
+            @include center-total;
+            flex-direction: column;
+            p, h3 {
+                margin-bottom: 10px;
+                color: white;
+            }
+            .price {
+                font-size: 30px;
+                font-weight: 400;
+            }
+        }
+
+        .header-links {
+            width: 100%;
+            @include center-align;
+            flex-direction: row;
+            justify-content: space-around;
+            margin-bottom: 10px;
+
+            .price-link {
+                width: 40%;
+                border: none;
+                font-size: 14px;
+            }
+
+            .disabled {
+                width: 40%;
+                background-color: transparent;
+                border: none;
+                color: rgba($color: #ffffff, $alpha: 0.5);
+                font-size: 14px;
+            }
+        }
+    }
+
+</style>
