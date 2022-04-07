c7a6e3094fb4579c2d4b6b2ddc46415d56d05973
Alexis Bally <alexisbally02@gmail.com>
2022-04-07 10:26:59 +0200

    Update error page

---
+++ layout/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HeaderHome />
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+import HeaderHome from "~/components/HeaderHome.vue";
+  export default {
+    target : 'static',
+    props: ["error"],
+    layout: "error"
+    ,
+    components: { HeaderHome }
+}
+</script>
